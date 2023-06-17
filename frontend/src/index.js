b60beabf53a4ab1a7442626a612fa02312ba2a15
ashishh-tomar <ashishtomar692@gmail.com>
2023-06-17 10:14:46 +0530

    Auth & User API Slice, Login Screen, Login functionality, User logout, User registeration.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,8 @@ import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
-
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +20,8 @@ const router = createBrowserRouter(
       <Route path='/' index={true} element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   )
 )
