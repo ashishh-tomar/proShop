aa6834cd4ca1f63f06427fdd06f6276fc56bce58
ashishh-tomar <ashishtomar692@gmail.com>
2023-06-18 20:11:50 +0530

    Removed some duplicate code which causing app to go live.

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,7 @@ app.use(express.urlencoded({extended:true}));
 //Cookie Parser Middle ware
 app.use(cookieParser());
 
-app.get('/', (req,res)=>{
-    res.send('API is running');
-});
+
 
 
 app.use('/api/products',productRoutes);
@@ -46,7 +44,7 @@ if(process.env.NODE_ENV === 'production'){
     //any route that is not api will be redirected to index.html 
     app.get('*',(req,res) =>
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    )
+    );
 }
 else{
     app.get('/', (req,res)=>{
